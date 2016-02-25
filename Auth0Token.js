c59576351dd026adc6676e5ba1563c6561556581
Sean Templeton <sean@templeton.io>
2016-02-25 11:34:48 -0700

    Switched to jsrassign library for token signing

--- Auth0Token.js
+++ Auth0Token.js
@@ -1,13 +1,4 @@
-loadScript('crypto-core.js');
-loadScript('crypto-hmac.js');
-loadScript('crypto-sha256.js');
-loadScript('crypto-base64.js');
-
-function getBase64Encoded(rawStr) {
-	var wordArray = CryptoJS.enc.Utf8.parse(rawStr);
-	var result = CryptoJS.enc.Base64.stringify(wordArray);
-	return result;
-}
+var jsrassign = require('./jsrassign');
 
 // Extensions are implemented as JavaScript classes
 var Auth0Token = function() {
@@ -15,28 +6,20 @@ var Auth0Token = function() {
   // implement the evaluate() method to generate the dynamic value
   this.evaluate = function() {
     var now = Math.floor((new Date()).getTime() / 1000),
-    	headerInput = JSON.stringify({
+    	headerInput = {
 			'typ': 'JWT',
 			'alg': 'HS256'
-		}),
-    	bodyInput = JSON.stringify({
-			'name': this.name,
+		},
+    	bodyInput = {
 			'email': this.email,
-			'email_verified': false,
 			'iss': this.issuer,
 			'sub': "auth0|" + this.userId,
 			'aud': this.clientId,
 			'exp': now + (60 * 60 * 24 * 7),
 			'iat': now
-		});
-		
-    var base64Header = getBase64Encoded(headerInput);
-    var base64Payload = getBase64Encoded(bodyInput);
+		};
 
-    var signature = CryptoJS.HmacSHA256(base64Header + "." + base64Payload, this.clientSecret);
-    var base64Sign = CryptoJS.enc.Base64.stringify(signature);
-
-    return 'Bearer ' + base64Header + "." + base64Payload + "." + base64Sign;
+    return 'Bearer ' + jsrassign.jws.JWS.sign(null, headerInput, bodyInput, {b64: jsrassign.b64utob64(this.clientSecret)});
   }
 }
 
@@ -51,7 +34,6 @@ Auth0Token.help = "https://Auth0.com";
 
 Auth0Token.inputs = [
 	DynamicValueInput("issuer", "Issuer", "String"),
-	DynamicValueInput("name", "Name", "String"),
 	DynamicValueInput("userId", "User Id", "String"),
 	DynamicValueInput("email", "User Email", "String"),
 	DynamicValueInput("clientId", "Client ID", "String"),
